3e2ba702bd7ef1031b98d36caacf5a8e0ae3ec43
Axel Gromat <axel.gromat@gmail.com>
2021-05-03 16:03:55 +0200

    Changed auth

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,12 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-
-import { getSortedPostsData } from '../lib/posts'
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Negi a fresh made Next.Js developper.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-        <p> Consult {' '}</p>
-        <Link href='/posts/first-post'>
-          <a>my first post here !</a>
-        </Link>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+import { useAuth } from '../lib/auth'
+export default function Index() {
+  const auth = useAuth()
+  return auth.user ? (
+    <div>
+      <p>Email: {auth.user.email}</p>
+      <button onClick={(e) => auth.signout()}>Sign Out</button>
+    </div>
+  ) : (
+    <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
   )
 }
